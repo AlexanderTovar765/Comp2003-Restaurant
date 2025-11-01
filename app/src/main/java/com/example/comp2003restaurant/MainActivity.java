bc6f943c8c48a32b143328c4bea9b0f1b164064e
AlexanderTovar765 <146185413+AlexanderTovar765@users.noreply.github.com>
2025-11-01 11:08:10 +0000

    Update MainActivity.java

--- app/src/main/java/com/example/comp2003restaurant/MainActivity.java
+++ app/src/main/java/com/example/comp2003restaurant/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+//This part is for declaring the button variables on the page
     private Button btnLogin, btnSignUp, btnFeedback;
 
     @Override
@@ -18,19 +18,19 @@ public class MainActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
         btnSignUp = findViewById(R.id.btnSignUp);
         btnFeedback = findViewById(R.id.btnFeedback);
-
+// This part is for the login button on the main page
         if (btnLogin != null) {
             btnLogin.setOnClickListener(v ->
                     startActivity(new Intent(MainActivity.this, LoginActivity.class))
             );
         }
-
+// This part is for the sign up button on the main page
         if (btnSignUp != null) {
             btnSignUp.setOnClickListener(v ->
                     startActivity(new Intent(MainActivity.this, SignupActivity.class))
             );
         }
-
+// This part is for the feedback button on the main page
         if (btnFeedback != null) {
             btnFeedback.setOnClickListener(v ->
                     startActivity(new Intent(MainActivity.this, FeedbackActivity.class))
